5a247e165f695d4e6456019f134c0a5830bcf0cf
asimayaz004 <asimayaz004@gmail.com>
2026-05-03 04:18:24 -0500

    Add files via upload

---
+++ output/prac2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class hero{
+    public:
+    int health;
+    char *name;
+
+    hero(){
+        cout<<"Constructor Called"<<endl;
+        // char name [20];
+    }
+    void print(){
+        cout<<"Name: "<<this->name<<", ";
+        cout<<"Health: "<<this->health<<endl;
+    }
+
+    void setHealth(int h){
+        health = h;
+    }
+
+    void setName(const char n[]){
+    name =new char [strlen(n) + 1];
+    strcpy(name, n);
+    }
+};
+
+int main(){
+    hero h1;
+    h1.setHealth(10);
+    h1.setName("Asim");
+    
+    hero h2(h1);
+    
+    cout<<"== Before =="<<endl;
+    h1.print();
+    h2.print();// shallow copy happens here;
+
+    h1.name[0]='D';
+    cout<<"== After =="<<endl;
+    h1.print();
+    h2.print();
+}
